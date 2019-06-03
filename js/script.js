0f40e6981c4fa2fbb24722c73d182ad90aaa3b10
Olegus <gulldan1996@gmail.com>
2019-06-03 16:05:25 +0300

    safe-call

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function safeCall (f) {
+  try {
+    f();
+    return true;
+  }
+  catch (e) {
+    return false;
+  }
+};
+
+console.log(safeCall(() => console.log('Hello!'))); // true
+console.log(safeCall(() => JSON.parse('abc'))); // false
+console.log(safeCall(() => false)); // true
+console.log(safeCall(() => abc)); // false
